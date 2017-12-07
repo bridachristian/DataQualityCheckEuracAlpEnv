6d72db5bd356396f23337289ca69955f7522f9b6
bridachristian <bridachristian@gmail.com>
2017-12-07 14:37:49 +0100

    updates

---
+++ R/structure_control.R
@@ -0,0 +1,11 @@
+structure_control = function(FILE_PATH = scheduling_dir,FILE_NAME = FILE,DATETIME_HEADER = DATETIME_HEADER, DATETIME_FORMAT = DATETIME_FORMAT){
+
+    input_data = read.csv(paste(FILE_PATH,FILE_NAME,sep = ""),header = F,stringsAsFactors = F)
+    
+    # check if there is row containing only NA
+  
+}
+
+data[!apply(data == "", 1, all),]
+
+FILE_NAME  = "yM4s.dat"

697dd0d6f5f347122aace54d0e2edec4a3a7d34a
bridachristian <bridachristian@gmail.com>
2017-12-13 17:27:55 +0100

    Updates download_table

--- Main_script/DQC_Multi_Files.R
+++ Main_script/DQC_Multi_Files.R
@@ -56,12 +56,24 @@ if(!file.exists(download_table_file)){
   first_download_table = data.frame(substring(files,1, nchar(files)-4), rep(NA, times = length(files)), rep(0,times = length(files)))
   colnames(first_download_table) = c("Station", "Last_date", "Stop_DQC")
   write.csv(first_download_table,download_table_file,quote = F,row.names = F)
+  file.copy(from = download_table_file, to = paste(substring(download_table_file,1,nchar(download_table_file)-4),"_old.csv",sep = ""),overwrite = TRUE)
+
+} else{
+  file.copy(from = download_table_file, to = paste(substring(download_table_file,1,nchar(download_table_file)-4),"_old.csv",sep = ""),overwrite = TRUE)
+  download_table = read.csv(download_table_file,stringsAsFactors = F)
+  station_to_process = substring(files,1, nchar(files)-4)
+  station_already_register = download_table$Station
+  station_to_add = setdiff(station_to_process,station_already_register)
+
+  df_to_add = data.frame(station_to_add, rep(NA, times = length(station_to_add)), rep(0,times = length(station_to_add)))
+  colnames(df_to_add) = c("Station", "Last_date", "Stop_DQC")
+
+  download_table = rbind(download_table, df_to_add )
 }
 
-file.copy(from = download_table_file, to = paste(substring(download_table_file,1,nchar(download_table_file)-4),"_old.csv",sep = ""),overwrite = TRUE)
-download_table = read.csv(download_table_file,stringsAsFactors = F)
-
-
+start_date = download_table$Last_date
+# file_to_analyze = download_table$Station[download_table$Stop_DQC == 0]
+# setdiff(station_to_process,station_already_register)
 
 # ..........................................................................................................................................................
 
@@ -70,9 +82,14 @@ download_table = read.csv(download_table_file,stringsAsFactors = F)
 # ..... rmarkdown render section .....................................................................................................................................
 
 for(i in 1: length(files)){
-  rmarkdown::render(input = paste(report_dir,"DQC_Manual_Multi_Files.Rmd",sep = ""),
-                    output_file = paste("DQC_Report_",substring(files[i],1,nchar(files[1])-4),".html",sep = ""),
-                    output_dir = paste(report_dir,"/Output_report/",sep = ""),
+  
+  input = paste(report_dir,"DQC_Manual_Multi_Files.Rmd",sep = "")
+  output_file = paste("DQC_Report_",substring(files[i],1,nchar(files[1])-4),".html",sep = "")
+  output_dir = paste(report_dir,"Output_report/",sep = "")
+  
+  rmarkdown::render(input = input,
+                    output_file = output_file,
+                    output_dir = output_dir,
                     params = list(file = files[i],
                                   scheduling_dir = scheduling_dir,
                                   report_dir = report_dir,
@@ -85,14 +102,33 @@ for(i in 1: length(files)){
                                   DATETIME_HEADER = DATETIME_HEADER,
                                   DATETIME_FORMAT = DATETIME_FORMAT,
                                   DATETIME_SAMPLING = DATETIME_SAMPLING,
-                                  RECORD_HEADER = RECORD_HEADER ))
+                                  RECORD_HEADER = RECORD_HEADER,
+                                  start_date = start_date[i]))
+  
+  last_date = mydata[nrow(mydata),which(colnames(mydata)== DATETIME_HEADER)]
+  download_table$Last_date[i] = last_date
+  
+  new_output_file = paste("DQC_Report_",substring(files[i],1,nchar(files[1])-4),"_",
+                          substring(last_date,1,4),
+                          substring(last_date,6,7),
+                          substring(last_date,9,10),
+                          substring(last_date,12,13),
+                          substring(last_date,15,16),".html",sep = "")
+  # if(!dir.exists(paste(output_dir,substring(files[i],1,nchar(files[1])-4),sep = ""))){
+  #   dir.create(paste(output_dir,substring(files[i],1,nchar(files[1])-4),sep = ""))
+  #   
+  # }
+  
+  file.rename(from = paste(output_dir,output_file,sep = "") ,to = paste(output_dir,new_output_file,sep = "") )
+  write.csv(download_table,download_table_file,quote = F,row.names = F)
 
-
-  rm(list=setdiff(ls(),c("scheduling_dir","report_dir","output_dir", "support_dir",
-                         "write_output","RANGE_FILE","DATA_FROM_ROW", "HEADER_ROW_NUMBER",
-                         "DATETIME_HEADER","DATETIME_FORMAT","DATETIME_SAMPLING", "RECORD_HEADER",
-                         "files", "i")))
 }
+
+#   rm(list=setdiff(ls(),c("scheduling_dir","report_dir","output_dir", "support_dir",
+#                          "write_output","RANGE_FILE","DATA_FROM_ROW", "HEADER_ROW_NUMBER",
+#                          "DATETIME_HEADER","DATETIME_FORMAT","DATETIME_SAMPLING", "RECORD_HEADER",
+#                          "files", "i")))
+
 
 # i=1
 # rmarkdown::render(input = paste(report_dir,"DQC_Manual_Multi_Files.Rmd",sep = ""),

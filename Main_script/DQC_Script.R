c86ab98d9a22dfe4a53e7deb7c93f354eea1e184
bridachristian <bridachristian@gmail.com>
2017-12-20 17:09:06 +0100

    updates

--- Main_script/DQC_Script.R
+++ Main_script/DQC_Script.R
@@ -7,117 +7,178 @@
 # Version:      1.0
 #
 #------------------------------------------------------------------------------------------------------------------------------------------------------
+remove(list=ls())
+Sys.setenv(TZ='Etc/GMT-1') # sets the environment on italy?s time zone
+
+# ..... Libraries .....................................................................................................................................
+
+library(devtools)
+install_github("bridachristian/DataQualityCheckEuracAlpEnv")
+library("DataQualityCheckEuracAlpEnv")
+
+library(zoo)
+library(timeSeries)
+library(knitr)
+library(ggplot2)
+library(dplyr)
+library(plyr)
+library(imputeTS)
+library(reshape2)
+library(kableExtra)
 
 # ..... Input section .................................................................................................................................
-# project_dir =  "H:/Projekte/Klimawandel/Experiment/data/2order/DataQualityCheckEuracAlpEnv/"
-# scheduling_dir <- "H:/Projekte/Klimawandel/Experiment/data/2order/DataQualityCheckEuracAlpEnv/Data/Input/"                     # <-- schelduling directory: for files to be processed
-# report_dir <- "H:/Projekte/Klimawandel/Experiment/data/2order/DataQualityCheckEuracAlpEnv/Report/"                   # <-- report directory: where to put reports and files whith overlaps
-# output_dir <- "H:/Projekte/Klimawandel/Experiment/data/2order/DataQualityCheckEuracAlpEnv/Data/Output/"               # <-- output directory: where processed files go
-# support_dir <- "H:/Projekte/Klimawandel/Experiment/data/2order/DataQualityCheckEuracAlpEnv/Data/Support_files/"       # <-- support directory: where to read support files
-# 
-# write_output <- TRUE                                             # if write_output == TRUE => write csv is admitted, if == FALSE not!
-# 
-# FILE <- "S3.dat"                                           # <-- Write here the file or the list of file  that you want to analyze!
-# 
-# RANGE_FILE = "Range.csv"
-# 
-# DATA_FROM_ROW <- 5                                             # <-- Row number of first data
-# HEADER_ROW_NUMBER <- 2                                         # <-- Row number of header
-# DATETIME_HEADER <- "TIMESTAMP"                                 # <-- header corresponding to TIMESTAMP
-# DATETIME_FORMAT <- "yyyy-mm-dd HH:MM"                          # <-- datetime format. Use only: y -> year, m -> month, d -> day, H -> hour, M -> minute
-# DATETIME_SAMPLING <- "15 min"
-# RECORD_HEADER <- "RECORD"                                      # <-- header corresponding to RECORD
-# 
-# start_date <- "2017-10-01 00:00"                                # <-- starting date to process file
+
+## Description: 
+## input_dir             # dir:  Directory where to put input files.
+## output_dir_data       # dir:  Directory where to save output files
+## output_dir_report     # dir:  Directory where to save output report
+## project_dir           # dir:  Directory of DataQualityCheckEuracAlpEnv package. Inside there are R script, Rmarkdown document and support files.
+## data_from_row         # num:  The row number of the first data row. (How many rows are dedicated to headers? + 1)
+## header_row_number     # num:  The row number of the header names
+## datetime_header       # chr:  The string to use to recognize datetime
+## datetime_format       # chr:  The string to use to recognize datetime. Use only: y -> year, m -> month, d -> day, H -> hour, M -> minute
+## datetime_sampling     # chr:  The string that define the time sampling (in POSIXct format)
+## record_header         # chr:  The string to use to recognize record header
+## range_file            # chr:  The string that indicate the name of range file saved in folder "project_dir/Data/Support_files/Range"
+## write_output_files    # log:  Logical status (TRUE/FALSE) to decide if save output data or not
+## write_output_report   # log:  Logical status (TRUE/FALSE) to decide if save output report or not
+## file                  # chr:  The string that indicate the name of file to process
+## start_date            # date: First date to consider for the analysis. Date before are not considered. No data is start_data <- NA
+  
+input_dir <- "H:/Projekte/Klimawandel/Experiment/data/2order/DQC_BrC_test_data/"
+output_dir_data <- "H:/Projekte/Klimawandel/Experiment/data/2order/DQC_BrC_test_data/test_output/Out_Data/"
+output_dir_report <- "H:/Projekte/Klimawandel/Experiment/data/2order/DQC_BrC_test_data/test_output/Out_Report/"
+project_dir <- "H:/Projekte/Klimawandel/Experiment/data/2order/DataQualityCheckEuracAlpEnv/"
+data_from_row =  5
+header_row_number =  2
+datetime_header =  "TIMESTAMP"
+datetime_format =  "yyyy-mm-dd HH:MM"
+datetime_sampling =  "15 min"
+record_header =  "RECORD"
+range_file =  "Range.csv"
+write_output_files =  "FALSE"
+write_output_report =  "FALSE"
+file <- "M3.dat"
+start_date <- "2017-11-23 05:45:00"
+
+# ~~~ Default directory ~~~~
+
+range_dir <- paste(project_dir, "Data/Support_files/Range/",sep = "")
+
+# .....................................................................................................................................................
 
 # ..... Body ..........................................................................................................................................
 
-# ---- test-a ----
 
-if(check_empty_file(SCHEDULING_DIR = scheduling_dir, FILE = FILE) == TRUE){
+if(check_empty_file(INPUT_DATA_DIR = input_dir, FILE_NAME = file) == TRUE){
   # writeLines(paste(FILE,"WARNING: NO DATA FOUND!!!",sep = " "))
   flag_empty = 1
 }else{
   
   flag_empty = 0
   
-  data_import <- read_data(FILE_PATH = scheduling_dir, FILE_NAME = FILE, 
-                           DATETIME_HEADER = DATETIME_HEADER, DATETIME_FORMAT = DATETIME_FORMAT,
-                           DATA_FROM_ROW = DATA_FROM_ROW, HEADER_ROW_NUMBER = HEADER_ROW_NUMBER)  
+  data_import <- read_data(INPUT_DATA_DIR = input_dir, FILE_NAME = file, 
+                           DATETIME_HEADER = datetime_header, DATETIME_FORMAT = datetime_format,
+                           DATA_FROM_ROW = data_from_row, HEADER_ROW_NUMBER = header_row_number)  
   header <- data_import [[1]]
   header_colnames <- data_import [[2]]
   data <- data_import [[3]]
+  flag_error_df <- data_import [[4]]
   
-  time_data = data[,which(colnames(data)==DATETIME_HEADER)]
-  
-  if(is.na(start_date)){
-    original <- data
-    mydata <- data
-  }else{
-    original <- data[which(time_data == as.POSIXct(start_date)):nrow(data),]
-    mydata <- data[which(time_data == as.POSIXct(start_date)):nrow(data),]
-  }
-  
-  deletes_duplcated <- deletes_duplcated_data(DATA = mydata,DATETIME_HEADER = DATETIME_HEADER)         # <- Deletes identical rows if found
-  mydata <- deletes_duplcated [[1]]                                                                                                        
-  duplicated_data <- deletes_duplcated [[2]]
-  duplicated_data <- time_to_char(DATA = duplicated_data, DATETIME_HEADER = DATETIME_HEADER, DATETIME_FORMAT = DATETIME_FORMAT)
-  
-  overlap <- detect_overlap(DATA = mydata,DATETIME_HEADER = DATETIME_HEADER, RECORD_HEADER = RECORD_HEADER)          # <- Detect overlap
-  
-  if(length(overlap) != 0){
+  if(flag_error_df == 0){
+    time_data = data[,which(colnames(data)==datetime_header)]
     
-    flag_overlap = 1
-    # stop(paste("Overlapping data in files:", FILE))
-    overlap[,1]<- overlap[,1] + DATA_FROM_ROW - 1
-    colnames(overlap)[1]= "File Row"
+    if(is.na(start_date)){
+      
+      original <- data
+      mydata <- data    
+      flag_date = 0
+      
+    }else{
+      
+      if(as.POSIXct(start_date) < time_data[length(time_data)]){
+        original <- data[(which(time_data == as.POSIXct(start_date))+1):nrow(data),]      # possible issues in data subset!!! to check 
+        mydata <- data[(which(time_data == as.POSIXct(start_date))+1):nrow(data),]
+        
+        flag_date = 0
+        
+      } else {
+        
+        flag_date = 1
+      }
+    }
     
-  }else{
     
-    flag_overlap = 0
-    
-    missing  <- missing_dates(DATA = mydata, DATETIME_HEADER = DATETIME_HEADER, RECORD_HEADER = RECORD_HEADER,DATETIME_SAMPLING = DATETIME_SAMPLING)  # <- fill missing dates with NA
-    mydata <- missing[[1]]
-    missing_index_date <- missing[[2]]
-    
-    range <- exclude_out_of_range(DATA = mydata,DATETIME_HEADER = DATETIME_HEADER, SUPPORT_DIR = support_dir, RANGE_FILE = RANGE_FILE) # <- Substitute with NA data out of phisical range
-    mydata <- range[[1]]
-    check_out_of_range <- range[[2]]
-    variable_not_in_range <-range[[3]]
-    
-    mydata <- time_to_char(DATA = mydata, DATETIME_HEADER = DATETIME_HEADER, DATETIME_FORMAT = DATETIME_FORMAT)
-    
+    if(flag_date == 0){
+      deletes_duplcated <- deletes_duplcated_data(DATA = mydata,DATETIME_HEADER = datetime_header)         # <- Deletes identical rows if found
+      mydata <- deletes_duplcated [[1]]                                                                                                        
+      duplicated_data <- deletes_duplcated [[2]]
+      duplicated_data <- time_to_char(DATA = duplicated_data, DATETIME_HEADER = datetime_header, DATETIME_FORMAT = datetime_format)
+      
+      overlap <- detect_overlap(DATA = mydata,DATETIME_HEADER = datetime_header, RECORD_HEADER = record_header)          # <- Detect overlap
+      
+      if(length(overlap) != 0){
+        
+        flag_overlap = 1
+        # stop(paste("Overlapping data in files:", FILE))
+        overlap[,1]<- overlap[,1] + data_from_row - 1
+        colnames(overlap)[1]= "File Row"
+        
+      }else{
+        
+        flag_overlap = 0
+        
+        missing  <- missing_dates(DATA = mydata, DATETIME_HEADER = datetime_header, RECORD_HEADER = record_header,DATETIME_SAMPLING = datetime_sampling)  # <- fill missing dates with NA
+        mydata <- missing[[1]]
+        missing_index_date <- missing[[2]]
+        
+        range <- exclude_out_of_range(DATA = mydata,DATETIME_HEADER = datetime_header, RANGE_DIR = range_dir, RANGE_FILE = range_file) # <- Substitute with NA data out of phisical range
+        mydata <- range[[1]]
+        check_out_of_range <- range[[2]]
+        variable_not_in_range_file <-range[[3]]
+        
+        mydata <- time_to_char(DATA = mydata, DATETIME_HEADER = datetime_header, DATETIME_FORMAT = datetime_format)
+        
+      }
+    }
   }
 }
 
 
 # ..... Output ..........................................................................................................................................
 
-if(write_output == TRUE){
-  
-  #~~~~~~~~~~
-  colnames(header) = header[1,]
-  
-  out_my = mydata
-  colnames(out_my) = colnames(header)
-  
-  out_mydata=rbind(header[-1,],out_my)
-  
-  out_filename =  paste(substring(mydata[nrow(mydata),which(colnames(mydata) == DATETIME_HEADER)],1,4),
-                        substring(mydata[nrow(mydata),which(colnames(mydata) == DATETIME_HEADER)],6,7),
-                        substring(mydata[nrow(mydata),which(colnames(mydata) == DATETIME_HEADER)],9,10),
-                        # "_",
-                        substring(mydata[nrow(mydata),which(colnames(mydata) == DATETIME_HEADER)],12,13),
-                        substring(mydata[nrow(mydata),which(colnames(mydata) == DATETIME_HEADER)],15,16),
-                        sep = "")
-  
-  
-  
-  write.csv(out_mydata,paste(output_dir,"DQCok_",out_filename,".csv",sep = ""),quote = F,row.names = F)
-  
-  #~~~~~~~~~~
-  colnames(duplicated_data) = colnames(header)
-  
-  out_duplicated_data=rbind(header[-1,],duplicated_data)
-  write.csv(out_duplicated_data,paste(output_dir,"Duplicated_",substring(FILE,1,nchar(FILE)-4),".csv",sep = ""),quote = F,row.names = F)
+if(flag_empty == 0){
+  if(flag_error_df == 0){
+    if(flag_date == 0){
+      if(flag_overlap == 0){
+        if(write_output_files == TRUE){
+          #~~~~~~~~~~
+          colnames(header) = header[1,]
+          
+          out_my = mydata
+          colnames(out_my) = colnames(header)
+          
+          out_mydata=rbind(header[-1,],out_my)
+          
+          out_filename = paste(substring(mydata[nrow(mydata),which(colnames(mydata) == datetime_header)],1,4),
+                               substring(mydata[nrow(mydata),which(colnames(mydata) == datetime_header)],6,7),
+                               substring(mydata[nrow(mydata),which(colnames(mydata) == datetime_header)],9,10),
+                               # "_",
+                               substring(mydata[nrow(mydata),which(colnames(mydata) == datetime_header)],12,13),
+                               substring(mydata[nrow(mydata),which(colnames(mydata) == datetime_header)],15,16),
+                               sep = "")
+          
+          
+          
+          write.csv(out_mydata,paste(output_dir_data,"DQCok_",substring(file,1,nchar(file)-4),"_",out_filename,".csv",sep = ""),quote = F,row.names = F)
+          
+          #~~~~~~~~~~
+          colnames(duplicated_data) = colnames(header)
+          
+          out_duplicated_data=rbind(header[-1,],duplicated_data)
+          write.csv(out_duplicated_data,paste(output_dir_data,"Duplicated_",substring(file,1,nchar(file)-4),"_",out_filename,".csv",sep = ""),quote = F,row.names = F)
+        }
+      }
+    }
+  }
 }

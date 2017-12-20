8fae5a7287df75aca763f0278e4b7a6aa6d1a922
bridachristian <bridachristian@gmail.com>
2017-12-20 10:00:24 +0100

    update path names

--- R/check_empty_file.R
+++ R/check_empty_file.R
@@ -1,7 +1,7 @@
 #' This function check the file size of input file
 #'
 #' @param INPUT_DATA_DIR Directory where input file is stored
-#' @param FILE File name (.dat)
+#' @param FILE_NAME File name (.dat)
 #'
 #' @return A logig value that indicates if the file is empty or not
 #' @export
@@ -9,9 +9,9 @@
 #' check_empty_file(INPUT_DATA_DIR = "~/Data/Input/",FILE = "M4s.dat")
 #' check_empty_file(INPUT_DATA_DIR = "Your input folder",FILE = "Your file to check")
 
-check_empty_file = function(INPUT_DATA_DIR,FILE){
+check_empty_file = function(INPUT_DATA_DIR,FILE_NAME){
 
-  info <- file.info(paste(INPUT_DATA_DIR,FILE, sep = ""))
+  info <- file.info(paste(INPUT_DATA_DIR,FILE_NAME, sep = ""))
   if(info$size == 0){
     empty <- TRUE
   }else{

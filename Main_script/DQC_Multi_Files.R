e06af9f8f337634d48ed1f5d1d01552182955db3
bridachristian <bridachristian@gmail.com>
2018-01-23 16:36:09 +0100

    Update report

--- Main_script/DQC_Multi_Files.R
+++ Main_script/DQC_Multi_Files.R
@@ -75,7 +75,7 @@ download_table = read_and_update_download_table(DOWNLOAD_TABLE_DIR = download_ta
 
 
 ############################################
-j = 4
+j = 1
 
 for(j in  1: length(files_available)){
   

44f8db22e955242f72030dca6ad55f86cd3533f6
bridachristian <bridachristian@gmail.com>
2018-05-24 08:57:19 +0200

    fix bugs

--- R/missing_record.R
+++ R/missing_record.R
@@ -25,31 +25,42 @@ missing_record = function(DATA ,DATETIME_HEADER = DATETIME_HEADER, RECORD_HEADER
   
   hhh = which(sign(record_diff-1) != 0)
   
-  d_st = data_new[hhh-1,which(colnames(data) == DATETIME_HEADER)]
-  d_en = data_new[hhh,which(colnames(data) == DATETIME_HEADER)]
-  r_st = data_new[hhh-1,which(colnames(data) == RECORD_HEADER)]
-  r_en = data_new[hhh,which(colnames(data) == RECORD_HEADER)]
   dd = matrix(nrow = length(hhh),ncol = 6)
   colnames(dd) = c("Datetime_From", "Datetime_To","Datetime_Missing","Record_From", "Record_To", "Record_Missing")
   dd = as.data.frame(dd)
   
-  for(i in 1:length(hhh)){
-    dd[i,1] = format(d_st[i],format = DATETIME_FORMAT)
-    dd[i,2] = format(d_en[i],format = DATETIME_FORMAT)   
-    dd[i,3] = length(seq(from = d_st[i],to = d_en[i],by = DATETIME_SAMPLING))-1
+  if(length(hhh) != 0){
     
-    dd[i,4] = format(r_st[i])
-    dd[i,5] = format(r_en[i])
-    dd[i,6] = data_new[hhh[i],which(colnames(data) == RECORD_HEADER)]-data_new[hhh[i]-1,which(colnames(data) == RECORD_HEADER)]
-  }
-  
-  dd
-  
-  if(length(hhh) == 0){
-    flag_missing_records = 0 
-  } else{
+    d_st = data_new[hhh-1,which(colnames(data) == DATETIME_HEADER)]
+    d_en = data_new[hhh,which(colnames(data) == DATETIME_HEADER)]
+    r_st = data_new[hhh-1,which(colnames(data) == RECORD_HEADER)]
+    r_en = data_new[hhh,which(colnames(data) == RECORD_HEADER)]
+    # dd = matrix(nrow = length(hhh),ncol = 6)
+    # colnames(dd) = c("Datetime_From", "Datetime_To","Datetime_Missing","Record_From", "Record_To", "Record_Missing")
+    # dd = as.data.frame(dd)
+    
+    for(i in 1:length(hhh)){
+      dd[i,1] = format(d_st[i],format = DATETIME_FORMAT)
+      dd[i,2] = format(d_en[i],format = DATETIME_FORMAT)   
+      dd[i,3] = length(seq(from = d_st[i],to = d_en[i],by = DATETIME_SAMPLING))-1
+      
+      dd[i,4] = format(r_st[i])
+      dd[i,5] = format(r_en[i])
+      dd[i,6] = data_new[hhh[i],which(colnames(data) == RECORD_HEADER)]-data_new[hhh[i]-1,which(colnames(data) == RECORD_HEADER)]
+    }
     flag_missing_records = 1
+  } else{
+    flag_missing_records = 0
   }
   
-  return(list(flag_missing_records,dd))
+  # if(length(hhh) == 0){
+  #   flag_missing_records = 0 
+  # } else{
+  #   flag_missing_records = 1
+  # }
+  # 
+  
+  l = list(flag_missing_records,dd)
+  names(l) = c("flag_missing_records", "data_table_missing_records")
+  return(l)
 } 

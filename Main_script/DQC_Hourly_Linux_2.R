f4812b03f076c8fca7307c79a5c1633b7686f480
bridachristian <bridachristian@gmail.com>
2018-06-18 18:46:31 +0200

    Fix bugs

--- Main_script/DQC_Hourly_Linux_2.R
+++ Main_script/DQC_Hourly_Linux_2.R
@@ -39,7 +39,7 @@ main_dir = "/shared/"
 
 project_type = c("LTER","MONALISA")
 
-PROJECT = "LTER" # Possible project: "LTER"; "MONALISA";
+PROJECT = "MONALISA" # Possible project: "LTER"; "MONALISA";
 # input_dir <- paste(main_dir,"/loggernet/scheduling_test/",sep = "")                    # where input files are
 
 input_dir <- paste(main_dir,"/Stations_Data/Data/LoggerNet_Raw_Data/Data/",sep = "")                    # where input files are
@@ -115,7 +115,9 @@ for(PROJECT in project_type){
     }
     
     if(PROJECT == "MONALISA"){                                                                        # <--Filter files based on Project (diffent if is MONALISA or LTER)
-      files_available = df_files$Files
+      # files_available = df_files$Files
+      files_available = df_files$Files[which(df_files$Datatable_name == "MeteoVal")]
+      
     }
   } else{
     files_available = files_no_project

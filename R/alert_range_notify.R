8672517a8d9babe2ab45c2fef12ebf294f6a0a70
bridachristian <bridachristian@gmail.com>
2018-10-17 14:26:35 +0200

    fix bugs

--- R/alert_range_notify.R
+++ R/alert_range_notify.R
@@ -152,7 +152,7 @@ alert_range_notify = function(DATA,DATETIME_HEADER = "TIMESTAMP",RECORD_HEADER, 
         }
         else{
           df_NA_tmp =  as.data.frame(matrix(ncol = 5, nrow = 0))
-          colnames(df_lower_tmp) = c("Variable", "From", "To", "Hours", "Mean_Value")
+          colnames(df_NA_tmp) = c("Variable", "From", "To", "Hours", "Mean_Value")
         }
         
         # new_status[,k] = ifelse(new[,k] < lower_limit, -1, new_status[,k])

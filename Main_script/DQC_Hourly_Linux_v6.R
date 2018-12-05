e7534ec55d2f7a30cf94c4eccaf0c560a328ea32
bridachristian <bridachristian@gmail.com>
2018-12-05 17:30:48 +0100

    updates

--- Main_script/DQC_Hourly_Linux_v6.R
+++ Main_script/DQC_Hourly_Linux_v6.R
@@ -77,7 +77,7 @@ DQC_setting_dir <- paste(main_dir,"/Stations_Data/DQC/",sep = "")
 
 logger_info_file <- paste(DQC_setting_dir,"/Process/Logger_number_and_software.csv", sep = "")
 range_dir <- paste(DQC_setting_dir,"/Process/", sep = "")
-download_table_dir <- paste(DQC_setting_dir,"/Process/", sep = "")
+download_table_dir <- paste(DQC_setting_dir,"/Process/Download_tables/Hourly/", sep = "")
 
 warning_report_RMD = paste(project_dir,"/Rmd/DQC_Warning_Reports.Rmd",sep = "")
 

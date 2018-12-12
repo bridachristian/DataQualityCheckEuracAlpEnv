5eeba50916999e095f5d822ce01212f15d903d4f
bridachristian <bridachristian@gmail.com>
2018-12-12 12:49:00 +0100

    folder cleaning

--- Main_script/hhh.R
+++
@@ -1,6 +0,0 @@
-# script per trasferire  dati da backup a loggernet pics
-
-backup = "/shared/test_christian/Stations_Data/Data/Pics_Backup/"
-loggernet = "/shared/test_christian/Stations_Data/Data/LoggerNet_Raw_Data/Data/Pics/"
-
-file.rename(from = backup,to = loggernet)

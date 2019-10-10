ee393f1c91b934ade3214b0b1d57a10b160b2b75
bridachristian <bridachristian@gmail.com>
2019-10-10 10:29:45 +0200

    Fix bug due to Error to readJPEG function

--- R/check_pics_corrupt.R
+++ R/check_pics_corrupt.R
@@ -6,17 +6,42 @@
 
 corr_pics = function(pics){
   pname = pics
-  p = readJPEG(pname,native = F)
+  corr = c()
+  i=1
+  # for(i in 1: length(pname)){
   
-  p2 = as.data.frame(p)
-  p3 = p2[(nrow(p2)/2):nrow(p2),]
-  
-  a_r = apply(X = p3,MARGIN = 1,FUN = unique)
-  if(length(unique(a_r)) == 1){
-    corr = TRUE
+  possibleError <- tryCatch(    # catch the possible error due  to wrong jpeg format (image corrupted)
+    expr ={
+      readJPEG(pname[i],native = F)
+    },
+    error = function(e) e
+  )
+  if(inherits(possibleError, "error")){
+    err = TRUE      # <- if there is an error err = TRUE , if no error in readJPEG  err = FALSE
   }else{
-    corr = FALSE
+    err = FALSE
   }
+  
+  
+  if(err == FALSE){  # <- if no error in readJPEG  err = FALSE read and check pics color, else the pics is cataloged as corrutped
+    # p = readJPEG(pname[i],native = F)
+    p = readJPEG(pname,native = F)
+    p2 = as.data.frame(p)
+    p3 = p2[(nrow(p2)/2):nrow(p2),]
+    
+    a_r = apply(X = p3,MARGIN = 1,FUN = unique)
+    if(length(unique(a_r)) == 1){
+      # corr[i] = TRUE
+      corr = TRUE
+    }else{
+      # corr[i] = FALSE
+      corr = FALSE
+    }
+  }else{
+    # corr[i] = TRUE
+    corr = TRUE
+  }
+  
   
   return(corr)
 }
